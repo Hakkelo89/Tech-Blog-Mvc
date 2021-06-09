2be56dd77019bd4e9ec32cd06532beca0b23a596
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-06-09 22:18:39 +0100

    render single post and add comment to post route

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,11 +3,21 @@ const handleSignupSubmit = () => {
   // /auth/sign-up
   // on success window location to /login
 };
-
+const handleLoginSubmit = () => {
+  // POST request with username and password
+  // /auth/login
+  // on success window location to /dashboard
+};
 const handleLogoutClick = () => {
   // POST request with username and password
   // /auth/logout
   // on success window location to /
+};
+
+const handleCommentSubmit = () => {
+  // POST request with comment message
+  // /api/posts/{postId}/comments
+  // on success window location to /posts/{postId}
 };
 
 console.log("client-side JS");
