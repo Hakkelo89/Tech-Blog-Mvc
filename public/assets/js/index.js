6d8c2ab6380ac281cc50def575f122d495b11c6e
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-06-23 17:27:13 +0100

    client side js for signup

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,20 +1,45 @@
-const handleSignupSubmit = () => {
-  // POST request with username and password
-  // /auth/sign-up
-  // on success window location to /login
-};
-
-const handleLoginSubmit = async (event) => {
+const handleSignupSubmit = async (event) => {
   event.preventDefault();
 
   const username = $("#username").val();
   const password = $("#password").val();
+  const confirmPassword = $("#confirmPassword").val();
 
+  if (password === confirmPassword) {
+    const requestBody = {
+      username: username,
+      password: password,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      body: JSON.stringify(requestBody),
+    };
+
+    const response = await fetch("/auth/sign-up", options);
+
+    if (response.status === 200) {
+      window.location.replace("/login");
+    } else {
+      console.log("Failed to signup");
+    }
+  } else {
+    console.log("Passwords do not match");
+  }
+};
+
+const handleLoginSubmit = async (event) => {
+  event.preventDefault();
+  const username = $("#username").val();
+  const password = $("#password").val();
   const requestBody = {
     username: username,
     password: password,
   };
-
   const options = {
     method: "POST",
     headers: {
@@ -23,15 +48,14 @@ const handleLoginSubmit = async (event) => {
     redirect: "follow",
     body: JSON.stringify(requestBody),
   };
-
   const response = await fetch("/auth/login", options);
 
   if (response.status === 200) {
-    // go to dashboard
     window.location.replace("/dashboard");
   } else {
     console.log("Failed to login");
   }
+};
 
 const handleLoginSubmit = () => {
   // POST request with username and password
@@ -65,3 +89,4 @@ const handlePostDelete = () => {
 
 console.log("client-side JS");
 $("#login-form").submit(handleLoginSubmit);
+$("#sign-up-form").submit(handleSignupSubmit);
