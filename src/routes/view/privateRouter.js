e80fb2d151c5037eb4945d462b8eac27967f5d5d
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-06-24 00:30:12 +0100

    refactor router to public and private

--- src/routes/view/privateRouter.js
+++ src/routes/view/privateRouter.js
@@ -1,15 +1,13 @@
 const { Router } = require("express");
 
-const renderDashboardPage = require("../../controllers/view/renderDashboardPage");
-const renderPostPage = require("../../controllers/view/renderPostPage");
-const renderCreatePostPage = require("../../controllers/view/renderCreatePostPage");
+const renderHomePage = require("../../controllers/view/renderHomePage");
+const renderLoginPage = require("../../controllers/view/renderLoginPage");
+const renderSignupPage = require("../../controllers/view/renderSignupPage");
 
 const router = Router();
 
-router.get("/dashboard", renderDashboardPage);
-
-router.get("/posts/:id", renderPostPage);
-
-router.get("/create-post", renderCreatePostPage);
+router.get("/sign-up", renderSignupPage);
+router.get("/login", renderLoginPage);
+router.get("/", renderHomePage);
 
 module.exports = router;
