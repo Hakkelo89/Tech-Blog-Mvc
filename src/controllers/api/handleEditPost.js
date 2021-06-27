8f5d1965056b2fba7d0bf758b4e2dacebe82bfb0
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-06-27 15:21:33 +0100

    handleEditPost refactored

--- src/controllers/api/handleEditPost.js
+++ src/controllers/api/handleEditPost.js
@@ -1,17 +1,5 @@
-const { Post } = require("../../models");
-
-const handleEditPost = async (req, res) => {
-  const { id } = req.params;
-  const { title, body } = req.body;
-
-  const updatedPost = {
-    title,
-    body,
-  };
-
-  await Post.update(updatedPost, { where: { id } });
-
-  res.status(200).json({ message: "success" });
+const handleEditPost = (req, res) => {
+  res.json({});
 };
 
 module.exports = handleEditPost;
