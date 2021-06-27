7d66f5b9f2acb5c764262b115bcbebaa2013f2e5
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-06-27 12:21:20 +0100

    Creat post refactored

--- src/controllers/api/handleCreatePost.js
+++ src/controllers/api/handleCreatePost.js
@@ -1,7 +1,13 @@
-const handleCreatePost = (req, res) => {
-  // get title and body from req body
-  // insert new post with userId
-  res.json("insert post here");
+const router = require("express").Router();
+const { Post } = require("../../models");
+
+const handleCreatePost = async (req, res) => {
+  const post = await Post.create({
+    ...req.body,
+    user_id: req.session.user_id,
+  });
+
+  res.status(200).json({ message: "Post created" });
 };
 
 module.exports = handleCreatePost;
